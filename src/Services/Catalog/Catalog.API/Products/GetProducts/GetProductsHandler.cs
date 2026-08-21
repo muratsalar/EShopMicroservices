1087d4adf95bf207cd4e1ad19b0352a1d334c0ce
muratsalar <murattsalar@gmail.com>
2026-08-21 21:41:44 +0300

    pagination health check and docker compose support

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,13 +1,15 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿using Marten.Pagination;
+
+namespace Catalog.API.Products.GetProducts
 {
-    public record GetProdcutsQuery: IQuery<GetProductsResult>;
+    public record GetProdcutsQuery(int? PageNumber = 1, int? PageSize = 10): IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
     internal class GetProductsQueryHandler(IDocumentSession session) 
         : IQueryHandler<GetProdcutsQuery, GetProductsResult>
     {
         public async Task<GetProductsResult> Handle(GetProdcutsQuery request, CancellationToken cancellationToken)
         {
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>().ToPagedListAsync(request.PageNumber ?? 1, request.PageSize ?? 10, cancellationToken);
             return new GetProductsResult(products);
         }
     }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,7 @@
 
 
 using BuildingBlocks.Behaviors;
+using HealthChecks.UI.Client;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,6 +31,8 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,5 +40,9 @@ var app = builder.Build();
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", 
+    new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse}
+    );
 
 app.Run();
